6ee2b35a7f07585ef3ecbd00af1abde099667af3
Hieu Le <letrunghieu.cse09@gmail.com>
2015-08-10 09:38:38 +0700

    First version of the plugin

---
+++ mysql-workbench-plugin-doc-generating.py
@@ -0,0 +1,16 @@
+from wb import *
+
+import grt
+
+ModuleInfo = DefineModule(name= "ModelDocumentExporter", author= "Hieu Le", version= "1.0.0")
+
+
+
+def printVersion():
+    print "Model Document Exporter v1.0.0 \nCopyright (c) 2015 Hieu Le"
+
+
+
+
+
+printVersion()
